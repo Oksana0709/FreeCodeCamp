ec146b33d8e1767987497d8b9bce0e92ecfcb28e
Oksana0709 <o07091980@gmail.com>
2023-11-25 22:44:30 +0300

    [tasks]

---
+++ Tasks/Selecting from ManyOptionsWithSwitchStatements.js
@@ -0,0 +1,26 @@
+//Write a switch statement which tests val and sets answer for the following conditions:
+// 1 - alpha
+// 2 - beta
+// 3 - gamma
+// 4 - delta
+
+function caseInSwitch(val) {
+    let answer = "";
+   switch (val) {
+     case 1:
+     return answer = 'alpha'
+     break;
+     case 2:
+        return answer ='beta'
+           break; 
+     case 3:
+        return answer ='gamma'
+           break;
+     case 4:
+      return answer ='delta'
+  
+   }
+    return answer;
+  }
+  
+  caseInSwitch(1);
